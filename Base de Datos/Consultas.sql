cca5d8d6fc4a26169c1e71ae0ddbc72705bc8a64
Karen Morán <kamoran.19@est.ucab.edu.ve>
2021-07-13 10:36:55 -0400

    Consultas definitivas

--- Base de Datos/Consultas.sql
+++ Base de Datos/Consultas.sql
@@ -7,7 +7,7 @@ SELECT
 FROM
 	clientes 
 WHERE
-	fecha_in > '2020-01-01' :: DATE 
+	EXTRACT ( YEAR FROM fecha_in ) = EXTRACT ( YEAR FROM CURRENT_DATE ) - 1 
 	AND fecha_out IS NOT NULL 
 ORDER BY
 	fecha_in,
@@ -21,22 +21,41 @@ SELECT DISTINCT
 FROM
 	clientes 
 WHERE
-	rif_cliente IN ( SELECT rif_cliente FROM facturas WHERE fecha_emision BETWEEN '2011-01-01' :: DATE AND '2020-01-01' :: DATE ) 
+	rif_cliente IN ( SELECT rif_cliente FROM facturas WHERE fecha_emision BETWEEN '2016-01-01' :: DATE AND '2019-01-01' :: DATE ) 
 ORDER BY
 	cedula DESC;
 
 --4
-SELECT cod_linea, descripcion,
-(SELECT COUNT (*) FROM articulos o WHERE o.cod_linea = l.cod_linea) AS cantidad_articulos,
-(SELECT AVG (precio) FROM articulos o WHERE o.cod_linea = l.cod_linea) AS precio_promedio
-FROM lineas l
-ORDER BY cantidad_articulos DESC;
+SELECT
+	cod_linea,
+	descripcion,
+	( SELECT COUNT ( * ) FROM articulos A WHERE A.cod_linea = l.cod_linea ) AS cantidad_articulos,
+	( SELECT AVG ( precio ) FROM articulos A WHERE A.cod_linea = l.cod_linea ) AS precio_promedio 
+FROM
+	lineas l 
+ORDER BY
+	cantidad_articulos DESC;
 
 --5
-SELECT DISTINCT num_factura, rif_cliente, fecha_emision
-FROM facturas
-WHERE num_factura IN (SELECT num_factura FROM facturas_detalles WHERE cod_articulo IN (SELECT cod_articulo FROM articulos WHERE cod_linea = (SELECT cod_linea FROM lineas WHERE descripcion = 'Articulos de papeleria')) AND fecha_emision  BETWEEN '2020-01-01'::DATE AND '2021-01-01'::DATE)
-ORDER BY rif_cliente, num_factura;
+SELECT DISTINCT
+	num_factura,
+	rif_cliente,
+	fecha_emision 
+FROM
+	facturas 
+WHERE
+	num_factura IN (
+	SELECT
+		num_factura 
+	FROM
+		facturas_detalles 
+	WHERE
+		cod_articulo IN ( SELECT cod_articulo FROM articulos WHERE cod_linea = ( SELECT cod_linea FROM lineas WHERE descripcion = 'Articulos de papeleria' ) ) 
+		AND EXTRACT ( YEAR FROM fecha_emision ) = EXTRACT ( YEAR FROM CURRENT_DATE ) - 1 
+	) 
+ORDER BY
+	rif_cliente,
+	num_factura;
 
 --6
 SELECT
@@ -48,12 +67,11 @@ SELECT
 	FROM
 		facturas_detalles f 
 	WHERE
-		cod_articulo = x.cod_articulo 
-		AND ( SELECT ( fecha_emision ) FROM facturas WHERE f.num_factura = num_factura ) BETWEEN '2011-01-01' :: DATE 
-		AND '2020-01-01' :: DATE 
+		cod_articulo = A.cod_articulo 
+		AND EXTRACT ( YEAR FROM ( SELECT ( fecha_emision ) FROM facturas WHERE f.num_factura = num_factura ) ) = EXTRACT ( YEAR FROM CURRENT_DATE ) - 1 
 	) AS total_ventas 
 FROM
-	articulos x 
+	articulos A 
 GROUP BY
 	cod_articulo 
 HAVING
@@ -63,30 +81,13 @@ HAVING
 	FROM
 		facturas_detalles f 
 	WHERE
-		cod_articulo = x.cod_articulo 
-		AND ( SELECT ( fecha_emision ) FROM facturas WHERE f.num_factura = num_factura ) BETWEEN '2011-01-01' :: DATE 
-		AND '2020-01-01' :: DATE 
+		cod_articulo = A.cod_articulo 
+		AND EXTRACT ( YEAR FROM ( SELECT ( fecha_emision ) FROM facturas WHERE f.num_factura = num_factura ) ) = EXTRACT ( YEAR FROM CURRENT_DATE ) - 1 
 	) > 5000000 
 ORDER BY
-	total_ventas
+	total_ventas;
 
 --7
-SELECT
-	cod_proveedor,
-	( SELECT ( razon_soc ) FROM proveedores WHERE cod_proveedor = x.cod_proveedor ) AS razon_social,
-	( SELECT ( direccion ) FROM proveedores WHERE cod_proveedor = x.cod_proveedor ) AS direccion,
-	( SELECT ( telefono ) FROM proveedores WHERE cod_proveedor = x.cod_proveedor ) AS telefono,
-	( SELECT ( status_p ) FROM proveedores WHERE cod_proveedor = x.cod_proveedor ) AS status,
-	cod_articulo,
-	( SELECT ( descripcion ) FROM articulos WHERE cod_articulo = x.cod_articulo ) AS nombre_articulo,
-	(precio / cantidad) AS precio_minimo 
-FROM
-	articulos_compras x 
-WHERE
-	cod_articulo = 'A0054' 
-	AND ( SELECT MIN ( precio / cantidad ) FROM articulos_compras WHERE cod_articulo = x.cod_articulo ) = precio / cantidad;
-
-
 SELECT DISTINCT
 	a_c.cod_proveedor,
 	P.razon_soc,
@@ -106,11 +107,49 @@ WHERE
 	a_c.cod_articulo = 'A0054' 
 	AND ( SELECT MIN ( precio / cantidad ) FROM articulos_compras WHERE cod_articulo = a_c.cod_articulo ) = a_c.precio / a_c.cantidad 
 	AND A.cod_articulo = a_c.cod_articulo 
-	AND P.cod_proveedor = a_c.cod_proveedor
+	AND P.cod_proveedor = a_c.cod_proveedor;
 
 --8
 UPDATE articulos A 
 SET status_a = 'D' 
 WHERE
 	existencia = 0 
-	AND ( SELECT MAX ( fecha_emision ) FROM facturas WHERE num_factura IN ( SELECT ( num_factura ) FROM facturas_detalles WHERE A.cod_articulo = cod_articulo ) ) < CURRENT_DATE - '1 YEAR' :: INTERVAL
+	AND ( SELECT MAX ( fecha_emision ) FROM facturas
+	WHERE num_factura IN ( SELECT ( num_factura ) FROM facturas_detalles
+	WHERE A.cod_articulo = cod_articulo ) ) < CURRENT_DATE - '1 YEAR' :: INTERVAL;
+
+--9
+CREATE TABLE IF NOT EXISTS historico_proveedores(
+	id SERIAL PRIMARY KEY,
+    cod_proveedor dom_codigo NOT NULL,
+    razon_soc varchar(50) NOT NULL,
+    direccion dom_direccion NOT NULL, 
+    telefono dom_telefono
+);
+
+CREATE OR REPLACE FUNCTION respaldar_prov ( ) RETURNS TRIGGER AS $historico$ BEGIN
+	INSERT INTO historico_proveedores ( cod_proveedor, razon_soc, direccion, telefono )
+	VALUES
+		( OLD.cod_proveedor, OLD.razon_soc, OLD.direccion, OLD.telefono );
+	DELETE 
+	FROM
+		articulos_compras 
+	WHERE
+		cod_proveedor = OLD.cod_proveedor;
+	RETURN OLD;
+	
+END;
+$historico$ LANGUAGE plpgsql;
+
+CREATE TRIGGER historico
+	BEFORE DELETE
+	ON proveedores
+	FOR EACH ROW
+	EXECUTE PROCEDURE respaldar_prov();
+
+
+DELETE 
+FROM
+	proveedores 
+WHERE
+	cod_proveedor IN ( SELECT ( cod_proveedor ) FROM proveedores WHERE status_p = 'E' );
